243c475b264fc094570afa1123ba1167f6a7ca3d
Thyvador <alex.hiltcher@gmail.com>
2022-05-22 02:03:13 +0200

    fix SupplyManagment page

--- src/component/tabs/forecast/ForecastTab.jsx
+++ src/component/tabs/forecast/ForecastTab.jsx
@@ -1,142 +1,168 @@
-import { TextField, MenuItem, Select, Paper, FormControl, InputLabel, useTheme } from '@material-ui/core';
+import {
+    TextField,
+    MenuItem,
+    Select,
+    Paper,
+    FormControl,
+    InputLabel,
+    useTheme,
+} from "@material-ui/core";
 import moment from "moment";
 import Map from "component/map/Map";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 import { useState } from "react";
 import LocationList from "./LocationList";
 import Charts from "./Charts";
 
-import locationService from 'services/location.service';
-
+import locationService from "services/location.service";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
+    root: {
+        "& > *": {
+            margin: theme.spacing(1),
+            width: "25ch",
+        },
     },
-  },
-  formContainer: {
-    padding: theme.spacing(2)
-  },
-  formTextField: {
-    marginRight: theme.spacing(1)
-  }, listContainer: {
-    padding: theme.spacing(1),
-    display: "flex"
-  }, placeList: {
-    flex: "2 1 0",
-    borderRightColor: theme.palette.divider,
-    borderRightWidth: "2px",
-    borderRightStyle: "solid",
-    paddingRight: theme.spacing(1)
-  }, placeChart: {
-    flex: "5 2 0",
-    paddingLeft: theme.spacing(1),
-    minHeight: "450px"
-  }
+    formContainer: {
+        padding: theme.spacing(2),
+    },
+    formTextField: {
+        marginRight: theme.spacing(1),
+    },
+    listContainer: {
+        padding: theme.spacing(1),
+        display: "flex",
+    },
+    placeList: {
+        flex: "2 1 0",
+        borderRightColor: theme.palette.divider,
+        borderRightWidth: "2px",
+        borderRightStyle: "solid",
+        paddingRight: theme.spacing(1),
+    },
+    placeChart: {
+        flex: "5 2 0",
+        paddingLeft: theme.spacing(1),
+        minHeight: "450px",
+    },
 }));
 
 /**
- * 
+ *
  * @param {Object} props
- * @param {Array} props.locations 
- * @returns 
+ * @param {Array} props.locations
+ * @returns
  */
 const ForecastTab = ({ locations }) => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [forecastRange, setForecastRange] = useState(60);
-  const [selectedLocation, setSelectedLocation] = useState(null);
-  const theme = useTheme();
+    const [forecastRange, setForecastRange] = useState(4);
+    const [selectedLocation, setSelectedLocation] = useState(null);
+    const theme = useTheme();
 
-  const [viewState, setViewState] = useState({
-    longitude: 7.048705213441168,
-    latitude: 43.612549661913825,
-    zoom: 1
-  });
+    const [viewState, setViewState] = useState({
+        longitude: 7.048705213441168,
+        latitude: 43.612549661913825,
+        zoom: 1,
+    });
 
-  // TODO :: use real date
-  const fromDate = moment();
+    // TODO :: use real date
+    const fromDate = moment();
 
-  const onSelectForecastRange = (event) => {
-    setForecastRange(event.target.value);
-  };
+    const onSelectForecastRange = (event) => {
+        setForecastRange(event.target.value);
+    };
 
-  const onSelectLocation = location => () => {
-    // setViewState({
-    //   longitude: location._source.lo.geolocation.longitude.value,
-    //   latitude: location._source.lo.geolocation.latitude.value,
-    //   zoom: 10
-    // })
-    setSelectedLocation(location);
+    const onSelectLocation = (location) => () => {
+        // setViewState({
+        //   longitude: location._source.lo.geolocation.longitude.value,
+        //   latitude: location._source.lo.geolocation.latitude.value,
+        //   zoom: 10
+        // })
+        setSelectedLocation(location);
+    };
 
-  }
+    return (
+        <>
+            <Paper
+                variant="outlined"
+                elevation={0}
+                className={classes.formContainer}
+            >
+                <TextField
+                    id="standard-basic"
+                    className={classes.formTextField}
+                    label="From"
+                    value={moment("2022-01-08T00:00:00").format("LLL")}
+                    disabled
+                />
+                <TextField
+                    id="standard-basic"
+                    className={classes.formTextField}
+                    label="To"
+                    value={moment("2022-01-01T00:00:00")
+                        .add(forecastRange, "week")
+                        .format("LLL")}
+                    disabled
+                />
 
-  return <>
-    <Paper variant="outlined" elevation={0} className={classes.formContainer}>
-
-      <TextField id="standard-basic" className={classes.formTextField} label="From" value={moment(fromDate).format("LLL")} disabled />
-      <TextField id="standard-basic" className={classes.formTextField} label="To" value={moment(fromDate).add(forecastRange, "days").format("LLL")} disabled />
-
-      <FormControl className={classes.formControl}>
-        <InputLabel id="forcast-range-label">Forecast: </InputLabel>
-        <Select
-          labelId="forcast-range-label"
-          id="demo-simple-select"
-          displayEmpty
-          value={"" + forecastRange}
-          onChange={onSelectForecastRange}
-        >
-          {/* <MenuItem value={0}>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="forcast-range-label">Forecast: </InputLabel>
+                    <Select
+                        labelId="forcast-range-label"
+                        id="demo-simple-select"
+                        displayEmpty
+                        value={"" + forecastRange}
+                        onChange={onSelectForecastRange}
+                    >
+                        {/* <MenuItem value={0}>
             <em>None</em>
           </MenuItem> */}
-          <MenuItem value={60}>4 weeks
-          </MenuItem>
-        </Select>
-      </FormControl>
+                        <MenuItem value={"4"}>4 weeks</MenuItem>
+                    </Select>
+                </FormControl>
+            </Paper>
 
-    </Paper>
+            <Map
+                disableModeSwitch
+                viewState={viewState}
+                onMove={(evt) => setViewState(evt.viewState)}
+                selected={selectedLocation?._id}
+                pointsData={(forecastRange === 0 ? [] : locations).map((l) => {
+                    return {
+                        id: l._id,
+                        coordinates: {
+                            latitude: l._source.lo.geolocation.latitude.value,
+                            longitude: l._source.lo.geolocation.longitude.value,
+                        },
+                        content: l._source.lo.locationName,
+                        color: locationService.getLocationColor(l, theme),
+                        textColor: theme.palette.common.black,
+                        radius: 20,
+                        onClick: onSelectLocation(l),
+                    };
+                })}
+            />
 
-    <Map
-      disableModeSwitch
-      viewState={viewState}
-      onMove={
-        evt => setViewState(evt.viewState)
-      }
-      selected={selectedLocation?._id}
-      pointsData={(forecastRange===0? [] :locations).map(l => {  
-        return {
-          id: l._id,
-          coordinates: {
-            latitude: l._source.lo.geolocation.latitude.value,
-            longitude: l._source.lo.geolocation.longitude.value,
-          },
-          content: l._source.lo.locationName,
-          color: locationService.getLocationColor(l, theme),
-          textColor: theme.palette.common.black,
-          radius: 20,
-          onClick: onSelectLocation(l)
-        }
-      })}
-    />
+            <Paper
+                variant="outlined"
+                elevation={0}
+                className={classes.listContainer}
+            >
+                <LocationList
+                    locations={forecastRange === 0 ? [] : locations}
+                    className={classes.placeList}
+                    selected={selectedLocation}
+                    onSelect={onSelectLocation}
+                />
 
-    <Paper variant="outlined" elevation={0} className={classes.listContainer}>
-      <LocationList locations={forecastRange===0? [] :locations} className={classes.placeList}
-        selected={selectedLocation}
-        onSelect={onSelectLocation}
-
-      />
-      
-      <div id="chart-container" className={classes.placeChart}>
-        <Charts location={selectedLocation} />
-      </div>
-    </Paper>
-
-
-  </>
-
-}
+                <div id="chart-container" className={classes.placeChart}>
+                    <Charts location={selectedLocation} />
+                </div>
+            </Paper>
+        </>
+    );
+};
 
 export default ForecastTab;
--- src/component/tabs/supplyManagement/SupplyManagement.jsx
+++ src/component/tabs/supplyManagement/SupplyManagement.jsx
@@ -1,165 +1,232 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Button, capitalize, Drawer, Grid, Icon, Input, makeStyles, Paper, TextField, Typography, useTheme } from "@material-ui/core";
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Box,
+    Button,
+    capitalize,
+    Drawer,
+    Grid,
+    Icon,
+    Input,
+    makeStyles,
+    Paper,
+    TextField,
+    Typography,
+    useTheme,
+    List,
+    ListItem,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { useEffect, useState } from "react";
-import ReactJson from 'react-json-view'
+import ReactJson from "react-json-view";
 import Timeline from "component/timeline/Timeline";
 import kuzzle from "services/kuzzle";
 import kuzzleService from "services/kuzzle/kuzzle.service";
-import moment from 'moment';
-
-const drawerWidth = 240;
+import moment from "moment";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: '33.33%',
-    flexShrink: 0,
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary,
-  },
-  drawerPaper: {
-    padding: theme.spacing(2),
-  }
+    root: {
+        width: "100%",
+    },
+    heading: {
+        // fontSize: theme.typography.pxToRem(15),
+        // flexBasis: "3 0 0",
+        // flexShrink: 0,
+        lineHeight: theme.typography.pxToRem(35),
+    },
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary,
+    },
+    drawerPaper: {
+        padding: theme.spacing(2),
+    },
 }));
 
 const SupplyManagement = ({ replenishments = [], events = [] }) => {
-  console.log(events)
+    const classes = useStyles();
 
-  const classes = useStyles();
+    const [trackedReplenishment, setTrackedReplenishment] = useState(null);
 
-  const [trackedReplenishment, setTrackedReplenishment] = useState(null);
+    if (replenishments.length === 0) {
+        return (
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+            >
+                <Grid item xs={12}>
+                    <Paper
+                        variant="outlined"
+                        elevation={0}
+                        className={classes.formContainer}
+                        style={{ minHeight: "450px" }}
+                    >
+                        No replenishments detected by the system
+                    </Paper>
+                </Grid>
+            </Grid>
+        );
+    }
 
-  const handleTntButtonClick = (replenishment) => (event) => {
-    event.stopPropagation();
-    setTrackedReplenishment(replenishment);
-  }
+    const forcasted = replenishments.filter(
+        (replenishment) => replenishment._source.status === "FORECASTED"
+    );
+    const accepted = replenishments.filter(
+        (replenishment) => replenishment._source.status !== "FORECASTED"
+    );
 
-  if (replenishments.length === 0) {
-    return <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justify="center"
-    >
-      <Grid item xs={12}>
-        <Paper variant="outlined" elevation={0} className={classes.formContainer} style={{ minHeight: "450px" }}>
-          No replenishments detected by the system
-        </Paper>
-      </Grid>
+    const eventGroups = [
+        {
+            departure: {
+                date: Date.parse("01 Jan 1970 00:00:01 GMT"),
+            },
+            arrival: {
+                date: Date.parse("31 Dec 2025 23:59:59 GMT"),
+            },
+        },
+    ];
 
-    </Grid>
+    const handleTntButtonClick = (replenishment) => (event) => {
+        console.log(event);
+        event.stopPropagation();
+        setTrackedReplenishment(replenishment);
+    };
 
-  }
-
-  const forcasted = replenishments.filter(replenishment => replenishment._source.status === "FORECASTED")
-  const accepted = replenishments.filter(replenishment => replenishment._source.status === "CONFIRMED" && !replenishment._source.proposal.deliveryDate)
-  const proposal = replenishments.filter(replenishment => replenishment._source.status === "CONFIRMED" && replenishment._source.proposal.deliveryDate)
-
-  const eventGroups = [{
-    departure: {
-      date: Date.parse('01 Jan 1970 00:00:01 GMT')
-    },
-    arrival: {
-      date: Date.parse('31 Dec 2025 23:59:59 GMT')
-    },
-  }]
-
-  return (
-    <Grid container spacing={3}>
-      <Grid item xs>
-      <div className={classes.root}>
-          <Typography variant="h4">Proposals</Typography>
-          <Box>
-
-            {proposal.map(replenishment => <SupplyManagementReplenishmentProposal key={replenishment._id} replenishment={replenishment} tntButtonAction={handleTntButtonClick(replenishment)} />)}
-          </Box>
-        </div>
-        <div className={classes.root}>
-          <Typography variant="h4">Forecasted</Typography>
-          <Box>
-
-            {forcasted.map(replenishment => <SupplyManagementReplenishmentForecasted key={replenishment._id} replenishment={replenishment} />)}
-          </Box>
-        </div>
-        <div className={classes.root}>
-          <Typography variant="h4">Accepted</Typography>
-          <Box>
-
-            {accepted.map(replenishment => <SupplyManagementReplenishmentAccepted key={replenishment._id} replenishment={replenishment} tntButtonAction={handleTntButtonClick(replenishment)} />)}
-          </Box>
-        </div>
-        <Paper >
-          <ReactJson theme="monokai" collapsed src={replenishments} />
-        </Paper>
-      </Grid>
-      {trackedReplenishment !== null && (
-        <Grid item xs={3}>
-          <Typography variant="h4">Track & Trace</Typography>
-          <Paper className={classes.drawerPaper}>
-            <ReactJson theme="monokai" collapsed src={events} />
-            <Timeline groups={eventGroups} events={[]} />
-            <Button variant="contained" color="secondary" onClick={() => setTrackedReplenishment(null)}>
-              Close
-            </Button>
-          </Paper>
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs>
+                {/* {forcasted.length > 0 && ( */}
+                <div className={classes.root}>
+                    <Typography variant="h4">Forecasted</Typography>
+                    <Box>
+                        {forcasted.map((replenishment) => (
+                            <SupplyManagementReplenishmentForecasted
+                                key={replenishment._id}
+                                replenishment={replenishment}
+                            />
+                        ))}
+                    </Box>
+                </div>
+                {/* )} */}
+                {accepted.length > 0 && (
+                    <div className={classes.root}>
+                        <Typography variant="h4">Accepted</Typography>
+                        <Box>
+                            {accepted.map((replenishment) => (
+                                <SupplyManagementReplenishmentAccepted
+                                    key={replenishment._id}
+                                    replenishment={replenishment}
+                                    onTntClick={handleTntButtonClick}
+                                />
+                            ))}
+                        </Box>
+                    </div>
+                )}
+            </Grid>
+            {trackedReplenishment !== null && (
+                <Grid item xs={3}>
+                    <Typography variant="h4">Track &and; Trace</Typography>
+                    <Paper className={classes.drawerPaper}>
+                        <Timeline groups={eventGroups} events={[]} />
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            onClick={() => setTrackedReplenishment(null)}
+                        >
+                            Close
+                        </Button>
+                    </Paper>
+                </Grid>
+            )}
         </Grid>
-      )}
-    </Grid>
-  );
-
-}
+    );
+};
 
 const SupplyManagementReplenishmentForecasted = ({ replenishment }) => {
+    const [quantity, setQuantity] = useState(replenishment._source.quantity);
 
-  const [quantity, setQuantity] = useState(replenishment._source.quantity)
+    const updateReplinishment = () => {
+        kuzzleService.acceptForecastedReplenishment(replenishment, quantity);
+    };
 
-  const updateReplinishment = () => {
-    kuzzleService.acceptForecastedReplenishment(replenishment, quantity);
-  }
+    return (
+        <ListItem
+            component={Paper}
+            style={{
+                display: "flex",
+                alignContent: "center",
+                justifyContent: "space-between",
+                padding: "1rem",
+            }}
+        >
+            <div style={{ display: "flex", alignContent: "center" }}>
+                <Typography>
+                    The system forcasted that you'll be in need of
+                </Typography>
+                <Typography style={{ fontWeight: "bold" }}>
+                    &nbsp;{replenishment._source.quantity} "
+                    {replenishment._source.containerSubType} containers"
+                </Typography>
+                .
+                <Typography style={{ textAlign: "right" }}>
+                    ({replenishment._id})
+                </Typography>
+            </div>
+            <div style={{ display: "flex", alignContent: "center" }}>
+                <TextField
+                    label="Quantity"
+                    variant="outlined"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                />
+                <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={updateReplinishment}
+                >
+                    Validate
+                </Button>
+            </div>
+        </ListItem>
+    );
+};
 
-  return <Accordion >
-    <AccordionSummary
-      expandIcon={<ExpandMoreIcon />}
-    >
-      <Typography >The system forcasted that you'll be in need of</Typography>
-      <Typography style={{ fontWeight: "bold" }}>&nbsp;{replenishment._source.quantity} "{replenishment._source.containerSubType} containers"</Typography>.
-      <Typography style={{ textAlign: 'right' }}>({replenishment._id})</Typography>
+const SupplyManagementReplenishmentAccepted = ({
+    replenishment,
+    onTntClick,
+}) => {
+    const classes = useStyles();
 
-
-
-    </AccordionSummary>
-    <AccordionDetails>
-      <TextField label="Quantity" variant="outlined" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-      <Button color="primary" variant="contained" onClick={updateReplinishment} >Validate</Button>
-    </AccordionDetails>
-  </Accordion>
-}
-
-const SupplyManagementReplenishmentAccepted = ({ replenishment, tntButtonAction }) => {
-  const classes = useStyles();
-
-  return <Accordion >
-    <AccordionSummary
-      expandIcon={<ExpandMoreIcon />}
-    >
-      <Typography className={classes.heading}>The system forcasted that you'll be in need of {replenishment._source.quantity}  {replenishment._source.containerSubType}  </Typography>
-      <Typography className={classes.secondaryHeading}>{replenishment._source.containerSubType}</Typography>
-      <Button variant="contained" color="primary" onClick={tntButtonAction}>
-        Track
-      </Button>
-    </AccordionSummary>
-    <AccordionDetails>
-      {replenishment._source.proposal.deliveryDate && "Valid"}
-      {!replenishment._source.proposal.deliveryDate && "No proposal yet"}
-    </AccordionDetails>
-  </Accordion>
-}
+    return (
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography className={classes.heading} variant="body1">
+                    The system forcasted that you'll be in need of{" "}
+                    {replenishment._source.quantity}{" "}
+                    {replenishment._source.containerSubType}{" "}
+                </Typography>
+                {/* <Typography className={classes.secondaryHeading}>
+                    {replenishment._source.containerSubType}
+                </Typography> */}
+            </AccordionSummary>
+            <AccordionDetails>
+                {replenishment._source.proposal && (
+                    <SupplyManagementReplenishmentProposal
+                        key={replenishment._id}
+                        replenishment={replenishment}
+                        onTntButton={onTntClick(replenishment)}
+                    />
+                )}
+                {!replenishment._source.proposal.deliveryDate &&
+                    "No proposal yet"}
+            </AccordionDetails>
+        </Accordion>
+    );
+};
 
 /**
  * proposal: {
@@ -172,19 +239,65 @@ const SupplyManagementReplenishmentAccepted = ({ replenishment, tntButtonAction 
         status: "PROPOSAL"
       },
  */
-const SupplyManagementReplenishmentProposal = ({ replenishment }) => {
-  const classes = useStyles();
+const SupplyManagementReplenishmentProposal = ({
+    replenishment,
+    onTntButton,
+}) => {
+    const classes = useStyles();
+    console.log(replenishment._source.proposal);
 
-  return <Accordion >
-    <AccordionSummary
-      expandIcon={<ExpandMoreIcon />}
-    >
-      <Typography className={classes.heading}>{replenishment._source.proposal.provider} proposes {replenishment._source.proposal.quantity} out of {replenishment._source.quantity} {replenishment._source.containerSubType} {replenishment._source.containerType} for {replenishment._source.proposal.price}$ in {moment(replenishment._source.proposal.deliveryDate.proposed).fromNow()} ({moment(replenishment._source.proposal.deliveryDate.proposed).toISOString()})</Typography>
-    </AccordionSummary>
-    <AccordionDetails>
-      <Button color="primary" variant="contained" onClick={() => kuzzleService.acceptProposal(replenishment._id)}>Accept the proposal ({replenishment._source.proposal.price}$)</Button>
-    </AccordionDetails>
-  </Accordion>
-}
+    return (
+        <Paper
+            style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "space-between",
+                alignContent: "center",
+                padding: "1rem",
+            }}
+            variant="outlined"
+        >
+            <Typography className={classes.heading}>
+                {replenishment._source.proposal.provider} proposes{" "}
+                {replenishment._source.proposal.quantity} out of{" "}
+                {replenishment._source.quantity}{" "}
+                {replenishment._source.containerSubType}{" "}
+                {replenishment._source.containerType} for{" "}
+                {replenishment._source.proposal.price}${" "}
+                {!!replenishment?._source?.proposal?.deliveryDate?.proposed
+                    ? "in " +
+                      moment(
+                          replenishment._source.proposal.deliveryDate.proposed
+                      ).fromNow() +
+                      " (" +
+                      moment(
+                          replenishment._source.proposal.deliveryDate.proposed
+                      ).toISOString() +
+                      ")"
+                    : ""}
+            </Typography>
+            {"COMPLETED" !== replenishment._source.proposal.status ? (
+                <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={() =>
+                        kuzzleService.acceptProposal(replenishment._id)
+                    }
+                >
+                    Accept the proposal ({replenishment._source.proposal.price}
+                    $)
+                </Button>
+            ) : (
+                <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={onTntButton}
+                >
+                    Track
+                </Button>
+            )}
+        </Paper>
+    );
+};
 
 export default SupplyManagement;
